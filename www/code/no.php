3199d9c52dbe7e4ece6c7132304b49da016d0f48
Rihards Lodzins <rihardslodzins@gmail.com>
2017-06-07 22:28:19 +0300

    test001

--- www/code/no.php
+++ www/code/no.php
@@ -5,7 +5,7 @@ setcookie("PHPSESSID","", time()-3600 );
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Test</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
