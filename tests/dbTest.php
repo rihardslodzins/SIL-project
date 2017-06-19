713f14f2a89b7be282a54ea7137468bc67decbe1
rihardslodzins <rihardslodzins@gmail.com>
2017-06-19 10:38:10 +0300

    Create dbTest.php

---
+++ tests/dbTest.php
@@ -0,0 +1,25 @@
+<?php
+use PHPUnit\Framework\TestCase; 
+abstract class DB_Connection_Test extends TestCase
+{
+static private $pdo= null;
+private $conn = null;
+private $rih = null;
+final public function getConnection()
+{
+    if ($this->conn === null)
+    {
+        if (self::$pdo == null)
+        {
+            self::$pdo = new PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+        }
+        $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
+    }
+    return $this->conn;
+}
+protected function getPdo()
+{
+    return $this->getConnection()->getConnection();
+}
+}
+?>
